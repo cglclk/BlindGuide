e36df85ce19508a11feace80b9f83ff8f9450dde
Cagla <ccelik@stud.hs-offenburg.de>
2024-03-15 02:55:30 +0100

    created Tabs with contentDescriptions

--- app/src/main/java/com/example/blindguide/ui/main/PageViewModel.kt
+++ app/src/main/java/com/example/blindguide/ui/main/PageViewModel.kt
@@ -1,16 +1,15 @@
 package com.example.blindguide.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
+
+//import androidx.lifecycle.ViewModelProvider
 
 class PageViewModel : ViewModel() {
 
     private val _index = MutableLiveData<Int>()
-    val text: LiveData<String> = Transformations.map(_index) {
-        "Hello world from section: $it"
+
+    val text: LiveData<String> = _index.map { index ->
+        "Hello world from section: $index"
     }
 
     fun setIndex(index: Int) {
